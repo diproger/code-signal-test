ba08573264eb1ea3ff72484184d3fef56c2585b5
a.saydumarov <a.saydumarov@smytsoft.com>
2024-04-25 12:37:56 +0500

    add tests

--- apps/transactions/tests.py
+++ apps/transactions/tests.py
@@ -1,3 +1,59 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Transaction
+from .serializers import TransactionSerializer
+from apps.wallets.models import Wallet
 
-# Create your tests here.
+
+class TransactionListViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.wallet = Wallet.objects.create(label='Test Wallet', balance=1000.0)
+        self.transaction_data = {
+            'wallet_id': self.wallet.id,
+            'txid': 'abc123',
+            'amount': 100.0
+        }
+        self.transaction = Transaction.objects.create(**self.transaction_data)
+
+    def test_get_transaction_list(self):
+        response = self.client.get('/api/transactions/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        transactions = Transaction.objects.all()
+        serializer = TransactionSerializer(transactions, many=True)
+        self.assertEqual(response.data, serializer.data)
+
+
+class TransactionDetailViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.wallet = Wallet.objects.create(label='Test Wallet', balance=1000.0)
+        self.transaction_data = {
+            'wallet_id': self.wallet.id,
+            'txid': 'abc123',
+            'amount': 100.0
+        }
+        self.transaction = Transaction.objects.create(**self.transaction_data)
+
+    def test_get_transaction_detail(self):
+        response = self.client.get(f'/api/transactions/{self.transaction.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        serializer = TransactionSerializer(self.transaction)
+        self.assertEqual(response.data, serializer.data)
+
+
+class TransactionCreateViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.wallet = Wallet.objects.create(label='Test Wallet', balance=1000.0)
+        self.transaction_data = {
+            'wallet_id': self.wallet.id,
+            'txid': 'abc123',
+            'amount': 100.0
+        }
+
+    def test_create_transaction(self):
+        response = self.client.post('/api/transactions/create/', self.transaction_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Transaction.objects.count(), 2)
--- apps/wallets/tests.py
+++ apps/wallets/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from apps.wallets.models import Wallet
+from apps.wallets.serializers import WalletSerializer
 
-# Create your tests here.
+
+class WalletListViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.wallet_data = {
+            'label': 'Test Wallet',
+            'balance': 1000.0
+        }
+        self.wallet = Wallet.objects.create(**self.wallet_data)
+
+    def test_get_wallet_list(self):
+        response = self.client.get('/api/wallets/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        wallets = Wallet.objects.all()
+        serializer = WalletSerializer(wallets, many=True)
+        self.assertEqual(response.data, serializer.data)
+
+
+class WalletDetailViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.wallet_data = {
+            'label': 'Test Wallet',
+            'balance': 1000.0
+        }
+        self.wallet = Wallet.objects.create(**self.wallet_data)
+
+    def test_get_wallet_detail(self):
+        response = self.client.get(f'/api/wallets/{self.wallet.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        serializer = WalletSerializer(self.wallet)
+        self.assertEqual(response.data, serializer.data)
+
+
+class WalletCreateViewTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.wallet_data = {
+            'label': 'New Wallet',
+            'balance': 500.0
+        }
+
+    def test_create_wallet(self):
+        response = self.client.post('/api/wallets/create/', self.wallet_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Wallet.objects.count(), 1)
